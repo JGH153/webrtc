d1bd85479953ee2b1e54551ee20ec3edee794688
Jan Greger Hemb <jan.greger@gmail.com>
2018-07-31 21:36:49 +0200

    And now we can draw properly

--- angular/src/app/drawing/drawing.component.ts
+++ angular/src/app/drawing/drawing.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit, ViewChild, AfterViewInit } from '@angular/core';
 
+interface Vec2D {
+	x: number;
+	y: number;
+}
+
 @Component({
 	selector: 'ARTC-drawing',
 	templateUrl: './drawing.component.html',
@@ -11,6 +16,11 @@ export class DrawingComponent implements OnInit, AfterViewInit {
 
 	private canvasRC: CanvasRenderingContext2D;
 
+	private lastPoint: Vec2D = null;
+
+	private isDrawing: boolean = false;
+	private mouseLeaveWhileDrawing: boolean = false;
+
 	constructor() { }
 
 	ngOnInit() {
@@ -18,34 +28,73 @@ export class DrawingComponent implements OnInit, AfterViewInit {
 	}
 
 	ngAfterViewInit() {
-
 		this.canvasRC = this.canvas.nativeElement.getContext('2d');
-
-		console.log(this.canvasRC);
-
-		// this.canvasRC.beginPath();
-		// this.canvasRC.arc(30, 30, 5, 0, 2 * Math.PI);
-		// this.canvasRC.closePath();
-		// this.canvasRC.fill();
-
-		// this.canvasRC.beginPath();
-		// this.canvasRC.arc(60, 60, 5, 0, 2 * Math.PI);
-		// this.canvasRC.closePath();
-		// this.canvasRC.fill();
-		// ctx.dr
 	}
 
-	onMouseMove(event) {
+	public setIsDrawing(newValue: boolean) {
+		this.isDrawing = newValue;
+		if (!newValue) {
+			this.lastPoint = null;
+		}
+	}
+
+	public onCanvasMouseDown(event) {
+		this.setIsDrawing(true);
 		const x = event.pageX - this.canvas.nativeElement.offsetLeft;
 		const y = event.pageY - this.canvas.nativeElement.offsetTop;
-		// console.log(event, x, y);
+		this.newDrawPoint(x, y);
+		event.preventDefault();
+	}
 
+	public onCanvasMouseRelease() {
+		this.setIsDrawing(false);
+	}
+
+	public onCanvasMouseEnter() {
+
+	}
+
+	public onCanvasMouseLeave() {
+		this.setIsDrawing(false);
+	}
+
+	public onMouseMove(event) {
+		if (!this.isDrawing) {
+			return;
+		}
+		const x = event.pageX - this.canvas.nativeElement.offsetLeft;
+		const y = event.pageY - this.canvas.nativeElement.offsetTop;
+		this.newDrawPoint(x, y);
+	}
+
+	public onTouchmove(event: TouchEvent ) {
+		const touch = event.touches[0];
+		const x = touch.pageX - this.canvas.nativeElement.offsetLeft;
+		const y = touch.pageY - this.canvas.nativeElement.offsetTop;
+		this.newDrawPoint(x, y);
+	}
+
+	public clear() {
+		this.canvasRC.clearRect(0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
+	}
+
+	private newDrawPoint(x, y) {
 		const radius = 5;
 
 		this.canvasRC.beginPath();
 		this.canvasRC.arc(x, y, radius, 0, 2 * Math.PI);
 		this.canvasRC.closePath();
 		this.canvasRC.fill();
+
+		if (this.lastPoint) {
+			this.canvasRC.beginPath();
+			this.canvasRC.moveTo(this.lastPoint.x, this.lastPoint.y);
+			this.canvasRC.lineTo(x, y);
+			this.canvasRC.lineWidth = radius * 2;
+			this.canvasRC.stroke();
+		}
+
+		this.lastPoint = {x, y};
 	}
 
 }
